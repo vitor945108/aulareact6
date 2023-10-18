5ef4a9261c3bbfe1359e02928d6a7bdbecb5a81b
vitor945108 <vitor.asantos31@senacsp.edu.br>
2023-10-18 11:59:30 -0300

    fist

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
+import TestList from './components/TestList/TestList';
+import Maca from "../src/images/maca.jpg";
+import Pera from "../src/images/pera.webp";
+import Uva from "../src/images/uva.webp";
+import ExercicioList from './components/ExercicioLista/ExercicioLista';
+import Equipe from './components/Equipe/Equipe';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
+      {/* {} */}
+    </div>
+  );
+}
+
+export default App;
